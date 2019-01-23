3db9c4e7441fd194517fbe1ed72ba5c9a326d774
Ryan Bigg <me@ryanbigg.com>
2019-01-24 10:16:13 +1100

    Change PUT to only update fields

--- spec/04_put_route_spec.rb
+++ spec/04_put_route_spec.rb
@@ -3,24 +3,25 @@ require "spec_helper"
 RSpec.describe "PUT /ratingQuestions/:id" do
   context "when the question exists" do
     let(:question) do
-      response = HTTP.post("#{SERVER}/ratingQuestions", json: { title: "Hello World" })
+      response = HTTP.post("#{SERVER}/ratingQuestions", json: { title: "Hello World", tag: "greetings" })
       response.parse
     end
 
-    let(:response) { HTTP.put("#{SERVER}/ratingQuestions/#{question["id"]}", json: { not_title: "Hello Mars" })}
+    let(:response) { HTTP.put("#{SERVER}/ratingQuestions/#{question["id"]}", json: { title: "Hello Mars" })}
 
     it "returns a 200 OK" do
       expect(response.status).to eq(200)
     end
 
-    it "updates the whole question, removing the old question" do
+    it "changes just the title attribute" do
       question = response.parse
       expect(question.is_a?(Hash)).to eq(true)
-      expect(question["not_title"]).to eq("Hello Mars")
+      expect(question["title"]).to eq("Hello Mars")
+      expect(question["tag"]).to eq("greetings")
     end
   end
 
-  context "asking to get a question that doesn't exist" do
+  context "asking to PUT a question that doesn't exist" do
     let(:response) do
       HTTP.put("#{SERVER}/ratingQuestions/i-will-never-exist")
     end
