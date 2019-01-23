2528f50bcdf15121a4f83eac07ec92adaa1ac793
Ryan Bigg <me@ryanbigg.com>
2019-01-23 15:44:51 +1100

    Create endpoint should return an error if no param is specified

--- spec/00_create_route_spec.rb
+++ spec/00_create_route_spec.rb
@@ -21,15 +21,13 @@ RSpec.describe "POST /ratingQuestions" do
   context "when the request has no body" do
     let(:response) { HTTP.post("#{SERVER}/ratingQuestions") }
 
-    it "returns a 201 Created" do
-      expect(response.status).to eq(201)
+    it "returns a 422 Invalid Resource" do
+      expect(response.status).to eq(422)
     end
 
     it "has no title attribute" do
-      question = response.parse
-      expect(question.is_a?(Hash)).to eq(true)
-      expect(question.key?("id")).to eq(true)
-      expect(question.key?("title")).to eq(false)
+      error = response.parse
+      expect(error).to eq({"errors" => [{"title" => "cannot be blank"}]})
     end
   end
 end
