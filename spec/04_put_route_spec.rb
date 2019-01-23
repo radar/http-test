9c7a4f54a399fc0d6548067a077ceb3991052acf
Ryan Bigg <me@ryanbigg.com>
2019-01-23 15:56:53 +1100

    Assert put removes extra field

--- spec/04_put_route_spec.rb
+++ spec/04_put_route_spec.rb
@@ -3,11 +3,11 @@ require "spec_helper"
 RSpec.describe "PUT /ratingQuestions/:id" do
   context "when the question exists" do
     let(:question) do
-      response = HTTP.post("#{SERVER}/ratingQuestions", json: { title: "Hello World" })
+      response = HTTP.post("#{SERVER}/ratingQuestions", json: { title: "Hello World", extra_field: "extra" })
       response.parse
     end
 
-    let(:response) { HTTP.put("#{SERVER}/ratingQuestions/#{question["id"]}", json: { not_title: "Hello Mars" })}
+    let(:response) { HTTP.put("#{SERVER}/ratingQuestions/#{question["id"]}", json: { title: "Hello Mars" })}
 
     it "returns a 200 OK" do
       expect(response.status).to eq(200)
@@ -16,7 +16,8 @@ RSpec.describe "PUT /ratingQuestions/:id" do
     it "updates the whole question, removing the old question" do
       question = response.parse
       expect(question.is_a?(Hash)).to eq(true)
-      expect(question["not_title"]).to eq("Hello Mars")
+      expect(question["title"]).to eq("Hello Mars")
+      expect(question.key?("extra_field")).to eq(false)
     end
   end
 
